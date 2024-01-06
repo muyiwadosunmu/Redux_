fe146eefafd3b3ecb862709a7784e011227dd5f2
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2024-01-06 13:55:46 +0100

    Created a reducer bank account

---
+++ src/store.jsx
@@ -0,0 +1,27 @@
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit": // They should model what happens
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return;
+      // LATER
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
